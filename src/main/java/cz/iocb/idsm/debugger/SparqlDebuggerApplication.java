7488bfd5d654f834cf849a1153077af5c473bb92
marek <moos@uochb.cas.cz>
2024-04-10 08:52:55 +0200

    Setup project

---
+++ src/main/java/cz/iocb/idsm/debugger/SparqlDebuggerApplication.java
@@ -0,0 +1,13 @@
+package cz.iocb.idsm.debugger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparqlDebuggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparqlDebuggerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/iocb/idsm/debugger/SparqlDebuggerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.iocb.idsm.debugger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparqlDebuggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
